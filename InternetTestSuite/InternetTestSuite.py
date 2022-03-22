9aad1ecd064ade4297caa1dd3844a49bd8c7a1ae
Seth <sethaplatt@gmail.com>
2022-03-22 01:14:07 -0500

    Add Settings class to handle config.ini management

--- InternetTestSuite/InternetTestSuite.py
+++ InternetTestSuite/InternetTestSuite.py
@@ -1,6 +1,25 @@
-import time, smtplib, configparser, _thread, speedtest
+import time, smtplib, os, configparser, _thread, speedtest, ssl
 from pythonping import ping
 from email.message import EmailMessage
+
+#Setting class to handle the read and management of the config.ini file
+class Settings:
+    def __init__(self, path):
+        self.path = path
+        
+        config = configparser.ConfigParser()
+        with open(configPath, 'r', encoding='utf-8') as f:
+            config.read_file(f)
+            self.address = config['Email']['Address']
+            self.password = config['Email']['Password']
+            self.ping_frequency = config['Application Config'].getfloat('Pint Frequency')
+            self.speedtest_frequency = config['Application Config'].getfloat('Speedtest Frequency')
+            self.email_frequency = config['Application Config']['Log Frequency']
+            self.email_day = config['Application Config']['Log Day']
+            self.email_time = config['Application Config']['Log Time']
+            
+        
+
 
 # If a ping to facebook or google make it though you have internet, otherwise you 
 # either don't have internet or something very unusual is happening.
@@ -18,4 +37,8 @@ def speedTest():
 
 
 
+configPath = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'config.ini').replace('\\','/')
+print(configPath)
+
+settings = Settings(configPath)
 
