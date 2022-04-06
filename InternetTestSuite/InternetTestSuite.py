def5efd5411846eb3f10c086d8914b4538b6329b
Seth Platt <sethaplatt@gmail.com>
2022-04-06 18:39:42 -0500

    Experiment with attaching a file to an email and inserting it in html.

--- InternetTestSuite/InternetTestSuite.py
+++ InternetTestSuite/InternetTestSuite.py
@@ -1,9 +1,14 @@
-import time, smtplib, os, configparser, ssl, csv, speedtest, matplotlib
+import time, smtplib, os, configparser, ssl, csv, speedtest, re, logging, base64
+from turtle import speed
+from matplotlib import pyplot as plot
+from io import BytesIO
 from filelock import FileLock
 from threading import *
 from pythonping import ping
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
 
 config_folder = os.path.dirname(os.path.realpath(__file__))
 
@@ -39,35 +44,45 @@ class connection_status:
     def connection_up(self):
         return self._connection_up
 
+    def toString(self) -> str:
+        return f'(connection_status: {{timestamp:{self.time}, connection_up: {self.connection_up}}})'
+
 # Simple class for speedtest result storage
 class speedtest_result:
     def __init__(self, time: float, download: float, upload: float) -> None:
-        self._time = time
-        self._download = download
-        self._upload = upload
+        self.__time = time
+        self.__download = download
+        self.__upload = upload
 
     @property
     def time(self):
-        return self._time
+        return self.__time
 
     @property
     def download(self):
-        return self._download
+        return self.__download
 
     @property
     def upload(self):
-        return self._upload
+        return self.__upload
+
+    def toString(self) -> str:
+        return f'(speedtest_result: {{timestamp:{self.time}, download: {self.download}, upload: {self.upload}}})'
+
         
 # If a ping to facebook or google make it though you have internet, otherwise you 
 # either don't have internet or something very unusual is happening.
 def checkConnection() -> connection_status:
     timeStamp = time.time()
     try:
-        if ((ping('facebook.com').success != False) or (ping('google.com').success != False)):
+        if ((ping('facebook.com').success) or (ping('google.com').success)):
             return connection_status(timeStamp, True)
         else:
             return (timeStamp, False)
     except RuntimeError as e:
+        if (re.search("^Cannot resolve address \".*\", try verify your DNS or host file$", str(e))):
+            return connection_status(timeStamp, False)
+        # Log the error here.
         return connection_status(timeStamp, False)
 
 # Execute a speedTest and return the results in Mbit/s as a tuple (Download, Upload)
@@ -109,26 +124,77 @@ def writeSpeedtestResults(results: speedtest_result):
 def constructObjectListFromCSV(file, constrution_function):
     lock = FileLock(file + '.lock')
     lock.acquire()
+    first_itr_flag = True
 
     with lock:
         if os.path.exists(file) and os.path.isfile(file):
             with open(file, 'r', newline='') as f:
                 reader = csv.reader(f)
                 return_objects = []
-                for i in range(1, len(reader)) :
-                    return_objects.append(constrution_function(reader[i]))
+                
+                for i in reader :
+                    if not first_itr_flag: 
+                        return_objects.append(constrution_function(i))
+                    else: first_itr_flag = not first_itr_flag
                 return return_objects
 
     lock.release()
 
+# Construction function for a connectionStatus object from a csv file line.
 def constructConnectionStatus(data: list) -> connection_status:
     return connection_status(data[0], data[1])
 
+# Construction function for a speedtest_result object from a csv file line.
 def constructSpeedtestResult(data: list) -> speedtest_result:
     return speedtest_result(data[0], data[1], data[2])
 
+# Construct a list of connecitonStatus objects from the stored csv file.
+def retrieveConnectionStatusData() -> list[connection_status]:
+    return constructObjectListFromCSV('connection_status.csv', constructConnectionStatus)
+
+# Construct a list of speedtest objects from the stored csv file.
+def retrieveSpeedtestData() -> list[speedtest_result]:
+    return constructObjectListFromCSV('speedtest_results.csv', constructSpeedtestResult)
+
+# Take in the file
+def imageFileToAttachment(filename: str):
+    p = MIMEBase('application', 'octet-stream')
+
+    p.set_payload(open(filename, 'rb').read())
+
+    encoders.encode_base64(p)
+    p.add_header('Content-Disposition', f'attachment; filename= {filename}.png')
+    p.add_header('Content-ID', f'<{filename}>')
+
+    return p
+
+# Save the plot as a file and return the file name minus the extension.
+def save_fig_to_file(plot, filename: str) -> str:
+    plot.savefig(f'{filename}.png')
+    return filename
+
 # Construct the and return a MIMEMultipart email message.
 def constructMessage(settings: Settings):
+    
+    connectionStatusData = retrieveConnectionStatusData()
+    speedtestData = retrieveSpeedtestData()
+
+    connectionStatusTimestamps = []
+    connectionStatusResults = []
+
+    speedtestResultTimestamps = []
+    speedtestResultDownloads = []
+    speedtestResultUploads = []
+
+    for c in connectionStatusData:
+        connectionStatusTimestamps.append(c.time)
+        connectionStatusResults.append(c.connection_up)
+
+    for s in speedtestData:
+        speedtestResultTimestamps.append(s.time)
+        speedtestResultDownloads.append(s.download)
+        speedtestResultUploads.append(s.upload)
+    
     message = MIMEMultipart('alternative')
 
     if settings.email_frequency == 'M' or settings.email_frequency == 'm':
@@ -144,9 +210,20 @@ def constructMessage(settings: Settings):
     message_body = f'''
     <center><h2>{settings.deployment_id} Connection Update</h2></center>
     <p>Connection health update.</p>
+    <body>
+        <div>
+        <img src="cid:speed_test" alt="Speed test graph" />
+        </div>
+    </body>
     '''
     
     message.attach(MIMEText(message_body, 'html'))
+
+    plot.plot(speedtestResultTimestamps, speedtestResultDownloads, label='Download Speed')
+    plot.plot(speedtestResultTimestamps, speedtestResultUploads, label='Upload Speed')
+
+    speedtestPlotImageFile = save_fig_to_file(plot, 'speed_test')
+    message.attach(imageFileToAttachment(speedtestPlotImageFile))
 
     return message
 
